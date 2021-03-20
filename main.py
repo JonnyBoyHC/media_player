af5df98217d68b38722e4664f8d776032783d928
JonnyBoy <jonnyboyhc@gmail.com>
2021-03-20 12:19:50 +0800

    Refactored some labels

--- main.py
+++ main.py
@@ -12,60 +12,60 @@ class Window(QWidget):
         super().__init__()
 
         # Set window interface
-        self.setWindowTitle("PyQt5 Media Player")
+        self.label = QLabel()
+        self.playBtn = QPushButton()
+        self.mediaPlayer = QMediaPlayer(None, QMediaPlayer.VideoSurface)
+        self.slider = QSlider(Qt.Horizontal)
+        self.setWindowTitle("COMP6125 - Group Assignment 1 - Media Player")
         self.setGeometry(350, 100, 700, 500)
         self.setWindowIcon(QIcon('player.png'))
 
         # Set Background to black color
-        p = self.palette()
-        p.setColor(QPalette.Window, Qt.black)
-        self.setPalette(p)
+        bg_color = self.palette()
+        bg_color.setColor(QPalette.Window, Qt.black)
+        self.setPalette(bg_color)
 
         self.init_ui()
         self.show()
 
     def init_ui(self):
         # Create media player object
-        self.mediaPlayer = QMediaPlayer(None, QMediaPlayer.VideoSurface)
 
         # Create videowidget object
         videowidget = QVideoWidget()
 
         # Create open button
-        openBtn = QPushButton('Open Video')
-        openBtn.clicked.connect(self.open_file)
+        open_btn = QPushButton('Open Video')
+        open_btn.clicked.connect(self.open_file)
 
         # Create button for playing
-        self.playBtn = QPushButton()
         self.playBtn.setEnabled(False)
         self.playBtn.setIcon(self.style().standardIcon(QStyle.SP_MediaPlay))
         self.playBtn.clicked.connect(self.play_video)
 
         # Create slider
-        self.slider = QSlider(Qt.Horizontal)
         self.slider.setRange(0, 0)
         self.slider.sliderMoved.connect(self.set_position)
 
         # Create label
-        self.label = QLabel()
         self.label.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Maximum)
 
         # Create hbox layout
-        hboxLayout = QHBoxLayout()
-        hboxLayout.setContentsMargins(0, 0, 0, 0)
+        hbox_Layout = QHBoxLayout()
+        hbox_Layout.setContentsMargins(0, 0, 0, 0)
 
         # Set widgets to the hbox layout
-        hboxLayout.addWidget(openBtn)
-        hboxLayout.addWidget(self.playBtn)
-        hboxLayout.addWidget(self.slider)
+        hbox_Layout.addWidget(open_btn)
+        hbox_Layout.addWidget(self.playBtn)
+        hbox_Layout.addWidget(self.slider)
 
         # Create vbox layout
-        vboxLayout = QVBoxLayout()
-        vboxLayout.addWidget(videowidget)
-        vboxLayout.addLayout(hboxLayout)
-        vboxLayout.addWidget(self.label)
+        vbox_Layout = QVBoxLayout()
+        vbox_Layout.addWidget(videowidget)
+        vbox_Layout.addLayout(hbox_Layout)
+        vbox_Layout.addWidget(self.label)
 
-        self.setLayout(vboxLayout)
+        self.setLayout(vbox_Layout)
         self.mediaPlayer.setVideoOutput(videowidget)
 
         # Media player signals
